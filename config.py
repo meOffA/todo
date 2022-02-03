ea15f4e6dbc0dca826bf8e381470f171ac3db9dc
Adam Nowok <78403731+meOffA@users.noreply.github.com>
2022-02-03 09:39:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,16 @@ class BaseConfig:
     DEBUG = False
     SECRET_KEY = '\xb2\xae\x00\x87\x00\xde\x16L\xa1PD\\\xe7\xcf\x8b\x11'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:dbtodo.database.windows.net,1433;Database=adamtodo123;Uid=adamtodo123;Pwd=Qwerty12#;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-
+    
 
 
 class DevelopmentConfig(BaseConfig):
     basedir = os.path.abspath(os.path.dirname(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:dbtodo.database.windows.net,1433;Database=adamtodo123;Uid=adamtodo123;Pwd=Qwerty12#;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-
-
+    
 class ProductionConfig(BaseConfig):
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:dbtodo.database.windows.net,1433;Database=adamtodo123;Uid=adamtodo123;Pwd=Qwerty12#;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-
-
+    
 
 config = {
     'dev': 'config.DevelopmentConfig',
